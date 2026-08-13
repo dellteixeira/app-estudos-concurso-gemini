29b3f4db2289326b9d52c4d568b69d7b4a4b0250
dellteixeira <111660903+dellteixeira@users.noreply.github.com>
2026-08-13 17:45:03 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'painel-estudos-v9-28-unified-study-state-20260813';
+const CACHE_NAME = 'painel-estudos-v9-29-dynamic-target-collapsed-flashcards-20260813';
 
 const APP_SHELL = [
   './manifest.json',
