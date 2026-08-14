264b5a98bd53f332241b2a9e2304fb906da3b8b4
dellteixeira <111660903+dellteixeira@users.noreply.github.com>
2026-08-13 22:29:06 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'painel-estudos-v9-33-adaptive-revisions-custom-hours-20260813';
+const CACHE_NAME = 'painel-estudos-v9-34-clean-study-checkboxes-20260813';
 
 const APP_SHELL = [
   './manifest.json',
