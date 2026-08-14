d1087824bc07ec496d320892d4e97dfdb990c5a3
dellteixeira <111660903+dellteixeira@users.noreply.github.com>
2026-08-14 10:04:29 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'painel-estudos-v9-39-drag-priority-20260814';
+const CACHE_NAME = 'painel-estudos-v9-40-pointer-reorder-export-materias-20260814';
 
 const APP_SHELL = [
   './manifest.json',
