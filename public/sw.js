05515df1fe54bb2b6c008f6a71e6be7bb7854595
dellteixeira <111660903+dellteixeira@users.noreply.github.com>
2026-08-14 23:53:32 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'painel-estudos-v9-61-active-recall-engine-20260815';
+const CACHE_NAME = 'painel-estudos-v9-62-retention-diagnostics-ux-20260815';
 
 const APP_SHELL = [
   './',
