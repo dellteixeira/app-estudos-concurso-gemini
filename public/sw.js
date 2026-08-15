6cf0b5bda5450fdd7c127ca3d1ad1a7e1ad45e73
dellteixeira <111660903+dellteixeira@users.noreply.github.com>
2026-08-15 12:17:41 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const APP_VERSION = '9.66.1';
-const CACHE_NAME = 'estudo-adaptativo-v9-66-1-prioridade-materias-20260815';
+const CACHE_NAME = 'estudo-adaptativo-v9-66-2-contabilizacao-horas-20260815';
 
 const APP_SHELL = [
   './',
