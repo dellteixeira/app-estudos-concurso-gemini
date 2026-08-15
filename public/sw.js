6fbb9822921b0f86a7fc6801c217f34c1d66b533
dellteixeira <111660903+dellteixeira@users.noreply.github.com>
2026-08-15 08:50:56 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const APP_VERSION = '9.66.0';
-const CACHE_NAME = 'estudo-adaptativo-v9-66-performance-consistencia-seguranca-20260815';
+const APP_VERSION = '9.66.1';
+const CACHE_NAME = 'estudo-adaptativo-v9-66-1-prioridade-materias-20260815';
 
 const APP_SHELL = [
   './',
