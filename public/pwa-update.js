c4cf7327cb71d74640c2bbe616603937963d2d22
dellteixeira <111660903+dellteixeira@users.noreply.github.com>
2026-08-15 22:39:31 -0300

    Update pwa-update.js

--- public/pwa-update.js
+++ public/pwa-update.js
@@ -1,4 +1,4 @@
-window.APP_VERSION = '9.66.8';
+window.APP_VERSION = '10.0.0';
       window.__pwaRegistration = null;
       window.__pwaUpdateRequested = false;
       window.__pwaUpdateCheckTimer = null;
