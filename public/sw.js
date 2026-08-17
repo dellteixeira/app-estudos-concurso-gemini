945373b82c30d3eac2b481bb45959343416cf744
dellteixeira <111660903+dellteixeira@users.noreply.github.com>
2026-08-16 22:26:36 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const APP_VERSION = '10.6.5';
-const CACHE_NAME = 'estudo-adaptativo-v10-6-5-retencao-ref05-20260816';
+const APP_VERSION = '10.6.6';
+const CACHE_NAME = 'estudo-adaptativo-v10-6-6-pontoscriticos-expansivel-20260816';
 
 const APP_SHELL = [
   './',
