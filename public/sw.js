3a1a5bbb42ea2470a7315824e9a5b9d8818a7c6a
dellteixeira <111660903+dellteixeira@users.noreply.github.com>
2026-08-18 23:54:43 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '10.14.2';
+const APP_VERSION = '10.14.3';
 const CACHE_PREFIX = 'estudo-adaptativo-';
 const CACHE_NAME = `${CACHE_PREFIX}v${APP_VERSION.replace(/\./g, '-')}`;
 
