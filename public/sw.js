a483f94841585f3dafa967f41b93fd77731e64fd
dellteixeira <111660903+dellteixeira@users.noreply.github.com>
2026-08-20 06:35:51 -0300

    feat(reader): integrate official PDF.js (public/sw.js)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '10.18.1';
+const APP_VERSION = '10.19.0';
 const CACHE_PREFIX = 'estudo-adaptativo-';
 const CACHE_NAME = `${CACHE_PREFIX}v${APP_VERSION.replace(/\./g, '-')}`;
 
